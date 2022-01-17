e85675bd1c2302b09f8483848b062f75e2806c39
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-01-17 05:38:59 +0000

    [Fix]users/index 部分テンプレート、本の新規投稿

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,16 @@ class User < ApplicationRecord
   has_many :books , dependent: :destroy
   has_one_attached :profile_image
 
-  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: {minimum: 2,maximum: 20}
+
 
   
   
-  def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  def get_profile_image(size)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+      profile_image.variant(resize: size).processed
   end
 end
