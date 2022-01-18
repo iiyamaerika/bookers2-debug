fb8232d249336298eec71a920d9e8c7384b1809f
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-01-18 14:58:50 +0000

    いいね機能、コメント機能を部分テンプレートに修正

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,23 +1,23 @@
 class PostCommentsController < ApplicationController
-  
+
   def create
     book = Book.find(params[:book_id])
     comment = PostComment.new(post_comment_params)
     comment.user_id = current_user.id
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
+    redirect_to request.referer
   end
-  
+
   def destroy
     PostComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    redirect_to request.referer
   end
-  
+
   private
-  
+
   def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
-  
+
 end
