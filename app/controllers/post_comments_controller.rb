2bfb85f6e565972376a67b3541aa94200ac5df2f
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-01-23 16:22:13 +0000

    コメント機能　非同期通信化

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -4,7 +4,7 @@ class PostCommentsController < ApplicationController
     @book = Book.find(params[:book_id])
     @post_comment = PostComment.new(post_comment_params)
     @post_comment.user_id = current_user.id
-    @post_comment.book_id = book.id
+    @post_comment.book_id = @book.id
     @post_comment.save
 
   end
